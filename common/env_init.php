62d146e3780306fd6c73ff676621b6a5f1f823aa
sTa4ever <guanhua2011@gmail.com>
2014-10-22 10:58:20 +0800

    fix

--- common/env_init.php
+++ common/env_init.php
@@ -15,6 +15,6 @@ define('APP_PATH', dirname(__FILE__) .'/..');
 $g_appIncludePath = APP_PATH .'/action/:'.
 				  APP_PATH .'/model/:' .
 				  APP_PATH .'/config/:' .
-                  APP_PATH .'/lib/:' .
+                  APP_PATH .'/lib/:';
 
 require_once APP_PATH . '/lib/config/PublicConfig.php';
